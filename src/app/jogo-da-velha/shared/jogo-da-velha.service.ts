570093248eff2a8a29988283bbd00695d466bd08
Silva <smfssilva@yahoo.com>
2021-09-05 14:11:31 -0300

    Implementando o servico 1/2

--- src/app/jogo-da-velha/shared/jogo-da-velha.service.ts
+++ src/app/jogo-da-velha/shared/jogo-da-velha.service.ts
@@ -5,5 +5,56 @@ import { Injectable } from '@angular/core';
 })
 export class JogoDaVelhaService {
 
-  constructor() { }
+  private readonly TAM_TAB: number = 3;
+  private readonly X: number = 1;
+  private readonly O: number = 2;
+  private readonly VAZIO: number = 0;
+
+  private tabuleiro: any;
+  private numMovimentos: number;
+  private vitoria: any;
+
+  private _jogador: number;
+  private _showInicio: boolean;
+  private _showTabuleiro: boolean;
+  private _showFinal: boolean;
+
+  constructor() {
+    this._showInicio = true;
+    this._showTabuleiro = false;
+    this._showFinal = false;
+    this.numMovimentos = 0;
+    this._jogador = this.X;
+    this.vitoria = false;
+    this.inicializarTabuleiro();
+  }
+
+  inicializarTabuleiro() {
+    this.tabuleiro = [this.TAM_TAB];
+    for (let i = 0; i < this.TAM_TAB; i++) {
+      this.tabuleiro[i] = [this.VAZIO, this.VAZIO, this.VAZIO];
+    }
+  }
+
+  get showInicio(): boolean {
+    return this._showInicio;
+  }
+
+  get showTabuleiro(): boolean {
+    return this._showTabuleiro;
+  }
+
+  get showFinal(): boolean {
+    return this._showFinal;
+  }
+
+  get jogador()  : number {
+    return this._jogador;
+  }
+
+  iniciarJogo(): void {
+    this._showInicio = false;
+    this._showTabuleiro = true;
+  }
+
 }
